ecf52f9f6aa49adcb3b3bc5319d4f63c406c4186
ShamilleD <shamilled@gmail.com>
2017-05-20 15:10:25 -0400

    all files

---
+++ sendemail.php
@@ -0,0 +1,29 @@
+<html>
+    <body>
+        <?php
+            if(isset($_POST['submit'])) {
+            
+                $to = "shamilled@gmail.com";
+                $email = $_POST['email'];
+                $name = $_POST['name'];
+                $subject = "New RHS Inquiry";
+                $msg = $POST_['message'];
+        
+                $message = "New Inquiry from: ".$name."\n"."Message: ".$msg;
+        
+                $sent = mail($to, $subject, $message, $headers);
+        
+                $headers = "From: $name";
+        
+        
+                if ($sent) {
+                    alert("Your inquiry has been sent! Please wait for a response.");
+                }
+                else {
+                    alert("We encountered a problem and could not send your message. Please try again.")
+                } 
+            }
+             
+        ?>
+    </body>
+</html>
